986e89a0911d2bdef74ac1267a5197f2671061e3
AtsukoShoji <136680855+AtsukoShoji@users.noreply.github.com>
2023-08-02 08:18:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 'use strict'
 // 1行目に記載している 'use strict' は削除しないでください
 
-//1-森の背景で、クリックするたびにちがう動物の画像とそのペアの値を表示させる
-//2-1の最後に選んだボタンによって物語を変える（クイズにする？）
-//右側に表示させたい文章が入った配列をつくる。（文だけだとわかりにくかったのでclass名も加えてオブジェクトにした）
+
+//★右側に表示させたい文章が入った配列をつくる。（文だけだとわかりにくかったのでclass名も加えてオブジェクトにした）
 const array = [
     {
         class: "bear", //使わないがわかりやすいように表示
@@ -36,7 +35,7 @@ const array = [
 ];
 
     
-//imgを取得
+//★imgを取得
 const picture = document.getElementsByTagName("img");
 //textのclassNameを取得
 const text = document.getElementsByClassName("text");
@@ -47,26 +46,25 @@ document.querySelector(".button2").style.visibility = "hidden";
 const button1 = document.querySelector(".button1");
 button1.addEventListener("click", getPicture);
 
-//クリック数は最初は0
+//★クリック数は最初は0
 let clickCount = 0;
 
-//ボタンを押したら絵と文字を表示させる関数を宣言
+//クリックしたら絵と文字を切り替える関数を宣言
 function getPicture(){ 
     console.log(clickCount);
     if (clickCount < 6){
         //右側に表示させる文字を変える（文字はひとつだけなので左辺のindexは0固定。
         text[0].innerHTML = array[clickCount].text;
-        //for-loopを使用で一旦すべての絵を非表示にする（ひとつ前の画像だけhiddenだとうまくいかなかった）
+        //★for-loopを使用で一旦すべての絵を非表示にする（ひとつ前の画像だけhiddenだとうまくいかなかった）
         for (let i = 0; i < array.length; i++){
             picture[i].style.visibility= "hidden";
         }
         //クリックした回数に対応した絵を表示させる
         picture[clickCount].style.visibility = "visible";
         
-        //うさぎの耳と鳥のしっぽ、恐竜の鼻？の所はボタン非表示にしたい
+        //★うさぎの耳と鳥のしっぽ、恐竜の鼻？の所はボタン非表示にしたい
         if (clickCount === 1 || clickCount === 3 || clickCount === 5){
             button1.style.visibility = 'hidden';
-            // document.querySelector(".button1").style.visibility = 'hidden';
         } 
         //処理順としては、次に うさ耳・鳥の尾 か恐竜の頭を押す（別のクリックイベント）
     //最後（恐竜がでた後）だったら、main1を非表示にしてmain2を出す
@@ -108,6 +106,14 @@ const tyranno = document.querySelector(".tyranno");
 tyranHead.addEventListener("click", tyranAppear);
 
 function tyranAppear(){
+  tyranno.animate( [ 
+    //★恐竜を動かす
+    {transform: "translateX(-100px)"},
+    {transform: "translateX(800px)"}
+    ],
+    {duration: 10000,
+    iterations:2
+    })  
     tyranHead.style.visibility = "hidden";
     button1.style.visibility = 'visible';
     //ボタンを「たっちしてね！」ではなく「にげる！」に変更
@@ -115,6 +121,7 @@ function tyranAppear(){
     document.querySelector(".button2").style.visibility = "visible";
     picture[clickCount].style.visibility = "visible";
     text[0].innerHTML = array[clickCount].text;
+
 
         document.getElementById("none").innerText = "←";
 };
@@ -136,4 +143,6 @@ back.addEventListener("click", () => {
 
 });
 //main2,main3つくる
+
+
 
